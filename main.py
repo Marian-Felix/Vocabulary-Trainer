005f92019fe0b363586fb9e572fac15ddd9543fe
Marian <marianfelix@aol.de>
2020-11-02 14:37:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
-import pandas as pd
-import os.path
-import add_vocab
-
-# read existing .csv table or create new one
-if not os.path.isfile('vocabulary.csv'):
-    vocab_table = pd.DataFrame(columns=['English', 'German1', 'German2', 'German3'])
-    vocab_table.to_csv('vocabulary.csv')
-    print("Initializing: Could not find existing table. Created new table.\n")
-
-else:
-    vocab_table = pd.read_csv('vocabulary.csv', index_col=[0])
-    print("Initializing: Found existing table with {} entries.\n".format(vocab_table.English.count()))
+from tkinter import *
+#import add_vocab
+from initialize import initialize
+from add_vocab import AddVocab
+from datetime import datetime
 
 
-# add new entries
-while vocab_table.English.count() < 2000:
 
-    # create single row dataframe from user input, add to existing dataframe
-    new_entry = add_vocab.user_input_to_df()
-    vocab_table = pd.concat([vocab_table, new_entry])
-    print("New entry added: \n", new_entry, "\n")
+window = Tk()
 
-    # add merged dataframe to .csv file
-    vocab_table.reset_index(inplace=True, drop=True)
-    vocab_table.to_csv('vocabulary.csv')
-    print("Current Table: \n", vocab_table, "\n")
+# initialize vocabulary table (load/create)
+vocab_table = initialize()
+
+window.title("Vocabulary Trainer")
+window.geometry('500x500')
+
+lbl = Label(window, text="Hello")
+lbl.config(font=('Arial', 44))
+lbl.place(relx=0.5, rely=0.4, anchor="center")
+
+lbl = Label(window, text="Current Mode ...")
+lbl.place(relx=0.5, rely=0.3, anchor='center')
+
+txt = Entry(window, width=18)
+txt.place(relx=0.5, rely=0.8, anchor="center")
+
+def clicked():
+    res = "Welcome to " + txt.get()
+    lbl.configure(text= res)
+
+def clicked_add():
+    lbl.configure(text= "Mode: \nAdd Vocabulary")
+
+def clicked_enter():
+    app = AddVocab()
+
+
+def clicked_excel():
+    dt_string = datetime.now().strftime("%d_%m_%Y %H-%M-%S")
+    vocab_table.to_excel('Vocabulary {}.xlsx'.format(dt_string))
+
+
+btn1 = Button(window, text="Add Vocabulary", command=clicked_add)
+btn2 = Button(window, text="(Remove Vocabulary)", command=clicked)
+btn3 = Button(window, text="(Practice!)", command=clicked)
+btn4 = Button(window, text="Create Excel", command=clicked_excel)
+btn5 = Button(window, text="    Enter    ", command=clicked_enter)
+
+btn1.grid(column=0, row=0)
+btn2.grid(column=1, row=0)
+btn3.grid(column=2, row=0)
+btn4.grid(column=3, row=0)
+
+btn5.place(relx=0.5, rely=0.85, anchor='center')
+
+window.mainloop()
